e89ab530f4eb4f4c116b9b6250d02a35ddff292b
therohanmehta <rohandhn786@gmail.com>
2023-05-04 09:43:07 +0530

    changes of menu bar and text area

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import DropDown from './Components/DropDown'
 import MenuBar from './MenuBar/MenuBar'
+import TextArea from './TextArea/TextArea'
 function App() {
 
   return (
     <>
       <MenuBar/>
-
+      <TextArea/>
     </>
   )
 }
---
+++ src/TextArea/TextArea.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import style from './TextArea.module.css'
+function TextArea() {
+    
+  return (
+    < >
+  
+    <br />
+    <br />
+    <div className={style.wholeArea}>
+        <divsta contentEditable className={style.txtArea}></divsta>
+      
+
+
+    </div>
+    </>
+  )
+}
+
+export default TextArea
