c528e4ceb861ce27a3c7eeaf59a6b6071687bb5a
Rishi-Kant <kant3397@gmail.com>
2023-05-06 01:01:32 +0530

    Added More buttons

--- src/atoms/button/Button.jsx
+++ src/atoms/button/Button.jsx
@@ -1,13 +1,57 @@
-
-
-import React from 'react'
-import style from "./Button.module.css"
-function Button({BiUndo, BiRedo,AiOutlinePrinter,MdSpellcheck,TfiPaintRoller,BsTypeBold,BiItalic,ImUnderline,MdOutlineFormatColorText,FaHighlighter}) {
+import React from "react";
+import style from "./Button.module.css";
+function Button({
+  BiUndo,
+  BiRedo,
+  AiOutlinePrinter,
+  MdSpellcheck,
+  TfiPaintRoller,
+  BsTypeBold,
+  BiItalic,
+  ImUnderline,
+  MdOutlineFormatColorText,
+  FaHighlighter,
+  MdOutlineInsertPhoto,
+  BiAlignLeft,
+  MdOutlineInsertLink,
+  BiCommentAdd,
+  MdFormatLineSpacing,
+  MdChecklist,
+  MdFormatListBulleted,
+  MdFormatListNumbered,
+  MdFormatIndentDecrease,
+  MdFormatIndentIncrease,
+  MdFormatClear,
+  handleBold,
+  handleColor
+}) {
   return (
     <div>
-      <button className={style.btn}>{BiUndo }{ BiRedo }{AiOutlinePrinter}{MdSpellcheck}{TfiPaintRoller}{BsTypeBold}{BiItalic}{ImUnderline}{MdOutlineFormatColorText}{FaHighlighter}</button>
+      <button className={style.btn}>
+        {BiUndo}
+        {BiRedo}
+        {AiOutlinePrinter}
+        {MdSpellcheck}
+        {TfiPaintRoller}
+        {BsTypeBold}
+        {BiItalic}
+        {ImUnderline}
+        {MdOutlineFormatColorText}
+        {FaHighlighter}
+        {MdOutlineInsertPhoto}
+        {BiAlignLeft}
+        {MdOutlineInsertLink}
+        {BiCommentAdd}
+        {MdFormatLineSpacing}
+        {MdChecklist}
+        {MdFormatListBulleted}
+        {MdFormatListNumbered}
+        {MdFormatIndentDecrease}
+        {MdFormatIndentIncrease}
+        {MdFormatClear}
+      </button>
     </div>
-  )
+  );
 }
 
-export default Button
+export default Button;
