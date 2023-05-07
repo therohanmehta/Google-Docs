3731a55c2890f440ea9907962257b01f27ea3bf6
SamadSRahman <121732577+SamadSRahman@users.noreply.github.com>
2023-05-08 02:39:28 +0530

    Font Size change added

--- src/IconBar/data.js
+++ src/IconBar/data.js
@@ -54,3 +54,33 @@ export const size = [
 export const fontSize = [
   1,2,3,4,5,6,7
 ]
+export const fontStyle = [
+  {
+    text: "Normal Text",
+    tag: "p"
+  },
+  {
+    text: "Title",
+    tag: "h1"
+  },
+  {
+    text: "Subtitle",
+    tag: "h4"
+  },
+  {
+    text: "Heading 1",
+    tag: "h2"
+  },
+  {
+    text: "Heading 2",
+    tag: "h3"
+  },
+  {
+    text: "Heading 3",
+    tag: "h5"
+  },
+  {
+    text: "Heading 4",
+    tag: "h6"
+  },
+]
